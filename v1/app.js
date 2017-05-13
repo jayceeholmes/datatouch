1f1913c495ecee70523148a04efb79742dc11cb4
jayceeholmes <jayceehermida@gmail.com>
2017-05-13 01:25:49 -0400

    White Space

--- v1/app.js
+++ v1/app.js
@@ -41,7 +41,7 @@ ctx.fillRect(500, 0, 780, 800);
 
 // TEXT
 ctx.font = "30px Arial";
-ctx.fillStyle = "white ";
+ctx.fillStyle = "white";
 ctx.fillText("Place your document in the", 605, 400);
 ctx.fillStyle = "red";
 ctx.fillText("viewing area", 975, 400);
