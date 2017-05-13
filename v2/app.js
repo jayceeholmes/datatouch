bfd4af99de18814d5de0578edd3f56fd8b99cda7
jayceeholmes <jayceehermida@gmail.com>
2017-05-13 16:38:17 -0400

    SIMPLE

--- v2/app.js
+++ v2/app.js
@@ -48,13 +48,49 @@ ctx.fillText("viewing area", 975, 400);
 // -----------------------------------------------------------------------------
 
 // CLASSIFIERS -----------------------------------------------------------------
-window.lampix.registerPositionClassifier([{
-    posX: 78, posY: 199, width: 344, height: 403, classifier: "document" // First Rect Position
-}], function (rectIndex, outlines) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear Canvas
-    ctx.fillStyle = "green";
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-}); // End Funcrion
+
+window.lampix.registerSimpleClassifier(
+    [{
+        // CLASS RECT ARRAY (1)
+        posX: 78,   // The X coordinate of the left side of the rectangle
+        posY: 199,   // The Y coordinate of the left side of the rectangle
+        width: 344,  // The width of the rectangle
+        height: 403, // The height of the rectangle
+        classifier: "cls_conv_fin" // The classifier to run inside the rectangle
+    }],
+
+    // Function that is called when a simpleClassifier is detected?
+    function (rectIndex, classTag) { // rectIndex: Which rect is it? || Why is classTag = 0
+        console.log("simpleClassifier", arguments);  // [RectIndex, classTag]
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear Canvas
+
+        // I was able to redefine the class tag and hit the other cases, but I have yet to see that happen in the simulator
+
+        switch(classTag){ // When do I get other cases?
+            case "finger": ctx.fillStyle = "red"; break;
+            case "no_finger": ctx.fillStyle = "grey";break;
+            case "1": ctx.fillStyle = "red"; break;
+            case "0": ctx.fillStyle = "blue"; break; // This is the case it is listening to
+            default: ctx.fillStyle = "white"; break; // WHEN DOES THIS HAPPEN?
+        }
+
+        if (rectIndex === 0) {
+            ctx.fillStyle = "green";
+            ctx.fillRect(0,0,canvas.width,canvas.height);
+        }
+
+); // End registerSimpleClassifier
+
+
+
+// window.lampix.registerPositionClassifier([{
+//     posX: 78, posY: 199, width: 344, height: 403, classifier: "document" // First Rect Position
+// }], function (rectIndex, outlines) {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear Canvas
+//     ctx.fillStyle = "green";
+//     ctx.fillRect(0,0,canvas.width, canvas.height);
+// }); // End Funcrion
 
 
 // -----------------------------------------------------------------------------
