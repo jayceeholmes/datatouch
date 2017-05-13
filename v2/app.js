e54ede568557d121b523c338740b2c811ffb79e2
jayceeholmes <jayceehermida@gmail.com>
2017-05-13 01:57:24 -0400

    SIMPLE

--- v2/app.js
+++ v2/app.js
@@ -47,21 +47,94 @@ ctx.fillText("viewing area", 975, 400);
 // -----------------------------------------------------------------------------
 
 // CLASSIFIERS -----------------------------------------------------------------
+window.lampix.registerSimpleClassifier(
+    [{
+            // DOCUMENT RECOGNITION
+            posX: 0, // The X coordinate of the left side of the rectangle
+            posY: 0, // The Y coordinate of the left side of the rectangle
+            width: 500, // The width of the rectangle
+            height: 800, // The height of the rectangle
+            classifier: "cls_conv_fin" // The classifier to run inside the rectangle
+    }], function(rectIndex, classTag) {
+        console.log("simpleClassifier", arguments); // [RectIndex, classTag]
 
-// POSITION
-window.lampix.registerPositionClassifier([{
-    posX: 78, posY: 199, width: 423, height: 343.6, classifier: "segm_cls_nes" // Document Position
-}], function (rectIndex, outlines) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear Canvas
 
-    // Here we need to change the outline of the object
-    // outlines[0].outline.points[0].posX = 100;
+        // SWITCH CASE
+        switch (classTag) {
+            case "finger":
+                ctx.fillStyle = "red";
+                break;
+            case "no_finger":
+                ctx.fillStyle = "grey";
+                break;
+            case "1":
+                ctx.fillStyle = "red";
+                break;
+            case "0":
+                ctx.fillStyle = "white";
+                break; // This is the case it is listening to
+            default:
+                ctx.fillStyle = "white";
+                break;
+        }
 
-    console.log("position classifier", rectIndex, outlines);
-    classifier_state[rectIndex] = outlines; // Classifier State just holds the object
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear Canvas
-    drawPDR(); // Draw
+        if (rectIndex === 0) {
 
-}); // END Function
+            // STUFF THAT IS DIFFERENT
+
+            // GREEN DOCUMENT
+            ctx.beginPath(); // Start Drawing this shape
+            ctx.rect(100, 100, 300, 600); // Paper Document
+            ctx.fill(); // Changes to color defined above
+            ctx.closePath(); // Close Shape
+
+            // OPTIONS: BOX
+            ctx.beginPath();
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = "red";
+            ctx.rect(600, 100, 580, 150); // Options Box
+            ctx.stroke();
+            ctx.closePath(); // Close Shape
+
+            // OPTIONS: PLOT
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(675, 175, 40, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath(); // Close Shape
+            ctx.fillText("PLOT",735,185);
+
+            // OPTIONS: TREND
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(925, 175, 40, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.closePath(); // Close Shape
+            ctx.fillText("TREND",990,185);
+
+
+            // STUFF THAT IS STILL THE SAME
+
+
+            // RED LINES
+            ctx.beginPath();
+            ctx.lineWidth = 10;
+            ctx.strokeStyle = "red";
+            ctx.moveTo(78, 199);
+            ctx.lineTo(78, 277);
+            ctx.moveTo(73, 199);
+            ctx.lineTo(153, 199);
+            ctx.moveTo(422, 602);
+            ctx.lineTo(422, 532);
+            ctx.moveTo(427, 602);
+            ctx.lineTo(352, 602);
+            ctx.stroke();
+            ctx.closePath();
+
+        } // END 0
+
+
 
 // -----------------------------------------------------------------------------
 
